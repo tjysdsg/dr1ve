253afc0f4f4435494f71b559c3a652e8adc65238
tjysdsg <tjy1018543509@163.com>
2021-02-16 21:18:35 +0800

    Load and display gltf car model

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 use bevy::prelude::*;
 
-fn hello_world() {
-    println!("hello world");
-}
-
 fn main() {
     App::build()
-        .add_system(hello_world.system())
+        .insert_resource(Msaa { samples: 4 })
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(commands: &mut Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn_scene(asset_server.load("porsche911/scene.gltf#Scene0"))
+        .spawn(LightBundle {
+            transform: Transform::from_xyz(10.0, 10.0, 10.0),
+            ..Default::default()
+        })
+        .spawn(PerspectiveCameraBundle {
+            transform: Transform::from_xyz(7.0, 7.0, 10.0)
+                .looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::unit_y()),
+            ..Default::default()
+        });
 }
